d93f89f626308723abfafd826e6001f5451bc891
dthwaite <dominicthwaites@mac.com>
2016-04-07 09:27:24 +0100

    Corrected travis test script & updated link to rawgit v1.0.11 in readme

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
-env ALL_BASES="yes" npm test
+env ALL_BASES="yes" mocha test/
 if [[ $TRAVIS_NODE_VERSION == 4* ]]; then
     env ALL_BASES="no" istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec
     cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
